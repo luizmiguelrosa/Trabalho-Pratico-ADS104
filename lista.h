b2955c6c1b9be9af724a576ecba1938e28b9f38f
luizmiguelrosa <luizmiguelrosalazarinirubio@gmail.com>
2024-05-27 16:05:44 -0300

    Adicionando funções de Lista Estática

---
+++ lista.h
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+typedef struct 
+{
+    void ** itens;
+    int tamanho_max;
+    int primeiro, ultimo;
+} Lista;
+
+void limpar(Lista* lista) {
+    lista->primeiro = 0;
+    lista->ultimo = lista->primeiro;
+}
+
+void inicializaLista(Lista* lista, int tamanho_max) {
+    lista->itens = (void**) malloc(sizeof(void*) * tamanho_max);
+    lista->tamanho_max = tamanho_max;
+    limpar(lista);
+}
+
+int vazia(Lista* lista) {
+    return (lista->primeiro == lista->ultimo);
+}
+
+int inserir(void* item, Lista* lista) {
+    if (lista->ultimo >= lista->tamanho_max)
+        return 0;
+    else {
+        lista->itens[lista->ultimo] = item;
+        lista->ultimo++;
+    }
+}
